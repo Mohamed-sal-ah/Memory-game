7280e30a4c36e329f8b822df533759a3bf371c76
Mohamed-sal-ah <mohamedsalahahmed@outlook.com>
2024-08-31 12:24:36 +0200

    changes to config file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: 'https://mohamed-sal-ah.github.io/Memory-game/' 
+  publicPath : process.env.NODE_ENV === 'production' ? '/Memory-game/' : '/',
+  transpileDependencies: true
 })
